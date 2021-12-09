8d9a09b4e7a923473153f181e5dd6154f21ee5da
xuewei cao <36172337+xueweic@users.noreply.github.com>
2021-12-09 11:39:38 -0500

    Add files via upload

---
+++ R/Sample_data.R
@@ -0,0 +1,11 @@
+#' @title Sample data for analysis
+#'
+#' @description Sample datasets for analysis using HuberNet and Net(TF, PWG, Annotation); Sample datasets for analysis using HuberLasso, Lasso, HuberENET, and ENET (TF, PWG).
+#'
+#' @format Datasets with 3 dataframes, which are:
+#' \describe{
+#' \item{TF} {Expression level for one TF with 50 sample size.}
+#' \item{PWG} {Expression levels for 131 PWGs with 50 sample size.}
+#' \item{Annotation} {Annotation file for 131 PWGs, which are included in 5 Pathways. (Only for HuberNet and Net methods)}
+#' }
+"Sample_data"
