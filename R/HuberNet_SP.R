0025fb6a2380f360c1e0d1b8be60ac3831236da4
xuewei cao <36172337+xueweic@users.noreply.github.com>
2023-02-21 17:43:47 -0500

    Update HuberNet_SP.R

--- R/HuberNet_SP.R
+++ R/HuberNet_SP.R
@@ -71,7 +71,7 @@ HuberNet_SP <- function(X, y, Adj, alphas, n_lambda, ratio=1e-2, B=500,  gamma=1
   SLS <- L_norm * diag(S)
   SLS <- t(SLS) * diag(S)
   # M: shape parameter
-  M <- IQR(t(X) %*% y)/1.345
+  M <- IQR(t(X) %*% y, na.rm = TRUE)/1.345
 
   ## Start Huber-Net by APGD method
   print("Start calculating selection probability using HuberNet by APGD method:")
